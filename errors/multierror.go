e8c3966fbbed2974fe0965ff88c0851f4afa0d2d
Ahmed (OneOfOne) W <oneofone@gmail.com>
2014-09-07 04:59:20 +0300

    converted MultiError to just a slice

--- errors/multierror.go
+++ errors/multierror.go
@@ -11,45 +11,46 @@ func (es ErrorString) Error() string {
 	return string(es)
 }
 
-type MultiError struct {
-	Errors []error
-}
+type MultiError []error
 
 func (me *MultiError) Append(err interface{}) (added bool) {
+	tmp := *me
 	added = true
 	switch err := err.(type) {
 	case error:
-		me.Errors = append(me.Errors, err)
+		tmp = append(tmp, err)
 	case string:
-		me.Errors = append(me.Errors, ErrorString(err))
+		tmp = append(tmp, ErrorString(err))
 	case []byte:
-		me.Errors = append(me.Errors, ErrorString(err))
+		tmp = append(tmp, ErrorString(err))
 	case *MultiError:
-		me.Errors = append(me.Errors, err.Errors...)
+		tmp = append(tmp, (*err)...)
 	case MultiError:
-		me.Errors = append(me.Errors, err.Errors...)
+		tmp = append(tmp, err...)
 	case []error:
-		me.Errors = append(me.Errors, err...)
+		tmp = append(tmp, err...)
 	case nil:
 		added = false
 	default:
 		added = false
 	}
+	*me = tmp
 	return
 }
 
 func (me *MultiError) Len() int {
-	return len(me.Errors)
+	return len(*me)
 }
 
 func (me *MultiError) Error() string {
-	if len(me.Errors) == 1 {
-		return me.Errors[0].Error()
+	tmp := *me
+	if len(tmp) == 1 {
+		return tmp[0].Error()
 	}
 
 	buf := &bytes.Buffer{}
 	buf.WriteString("Multiple Errors Found:\n")
-	for i, e := range me.Errors {
+	for i, e := range tmp {
 		fmt.Fprintf(buf, "\t[%d] %s\n", i, e.Error())
 	}
 	return buf.String()[:buf.Len()-1] // skip the last \n
